e335d8efd5ae2f20d8f9cc70e94b2dbfd02597b7
tpwright <tpwright@ncsu.edu>
2019-04-17 08:52:02 -0400

    creating hardware dir

--- Arduino/TandRHUsingMQTT/TandRHUsingMQTT.ino
+++ Arduino/TandRHUsingMQTT/TandRHUsingMQTT.ino
@@ -1,5 +1,6 @@
-//**************************************************
-//**************************************************
+/**************************************************
+ * 
+ *************************************************/
 #include <ESP8266WiFi.h>  // ESP8266 Wifi library
 #include <PubSubClient.h> // MQTT library
 
@@ -14,6 +15,8 @@
 // Sensor board library
 #include "SparkFun_Si7021_Breakout_Library.h"
 
+// 'Rename WifiSettingsTemplate.h' file to 'WifiSettings.h' 
+//  and edit file with appropriate wifi settings
 #include "WifiSettings.h"
 //**************************************************
 // Static location of my MQTT server
@@ -24,6 +27,7 @@
 #define NTP_INTERVAL 60 * 1000    // In miliseconds
 #define NTP_ADDRESS  "us.pool.ntp.org"  
 //**************************************************
+// Global variables
 static float RH;
 static float T;
 
@@ -33,30 +37,119 @@ static unsigned long lastSync;
 static byte mac[6];
 static char myName[] = "xxxx\0";
 
-bool pushButtonPressed = false;
-bool ledState = false;
+static bool pushButtonPressed = false;
+static bool ledState = false;
+static byte ledToggleState = 1;
 
 //**************************************************
+// Hardware parameters 
 Weather sensor;
 const byte ledPin = 13;
 const byte pushButtonPin = 14;
 //**************************************************
+// Network declarations
 WiFiClient espClient;
 PubSubClient client(espClient);
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, NTP_ADDRESS, NTP_OFFSET, NTP_INTERVAL);
 
+//**************************************************
+// Internet time
+static unsigned long epochTime;
+static time_t localTime, utc;
+static TimeChangeRule usEDT = {"EDT", Second, Sun, Mar, 2, -300};  //UTC - 5 hours - change this as needed
+static TimeChangeRule usEST = {"EST", First, Sun, Nov, 2, -360};   //UTC - 6 hours - change this as needed
+Timezone usEastern(usEDT, usEST);
+const char * ampm[] = {"AM", "PM"} ;
+void establishLocalTime() {
+  if (WiFi.status() == WL_CONNECTED) { 
+    Serial.println("Getting internet time");  
+    timeClient.update();
+    epochTime =  timeClient.getEpochTime();
+    utc = epochTime;
+    localTime = usEastern.toLocal(utc);
+    Serial.print("Current time is ");Serial.print(hourFormat12(localTime));
+    Serial.print(":");Serial.print(minute(localTime));
+    Serial.print(":");Serial.print(second(localTime));
+    Serial.print(" ");Serial.println(ampm[isPM(localTime)]);
+    lastSync = currentms/1000;
+  }
+  else
+    Serial.println("No internet connection");
+}
 
+//**************************************************
+// Helper function to convert mac address bytes to chars
+char convertHexToChar(byte in) {
+  if (in < 10)
+    return in+48;
+   else
+    return in+55;
+}
+
+//**************************************************
+// Called via pushbutton interrupt, using latch logic
+//  mechanism to prevent switch bounce from interfering 
+void capturePushButton() {
+  pushButtonPressed = true;
+}
+
+//**************************************************
+// MQTT reconnect function.
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+      // Once connected subscribe to topics
+      client.subscribe("ledToggle");
+      client.subscribe("reset");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+//**************************************************
+// Called when MQTT message arrives
+void callback(char* topic, byte* thisPayload, unsigned int lPayload) {
+  // ESP.restart();
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  if (topic[0] == 'r' && topic[1] == 'e' && topic[2] == 's' && topic[3] == 'e' && topic[4] == 't')
+  {
+    Serial.println("Reseting ESP in 3 seconds...");
+    delay(3000);
+    ESP.restart();
+  }
+  else {
+    for (int i = 0; i < lPayload; i++) {
+      char receivedChar = (char)thisPayload[i];
+      Serial.print(receivedChar);
+      if (receivedChar == '0')
+        ledToggleState = 0;
+      if (receivedChar == '1')
+        ledToggleState = 1;
+    }
+    Serial.println();
+  }
+}
+
+//=============================================================
 void setup() {
   // Establish arduino specifics
   Serial.begin(9600);
-
   pinMode(ledPin, OUTPUT);
   attachInterrupt(digitalPinToInterrupt(pushButtonPin), capturePushButton, CHANGE);
 
   // Establish wifi credentials
-  // Rename template file to 'WifiSettings.h' and edit file with appropriate 
-  //  wifi settings
   const char* wifiName = MyWifiName;
   const char* wifiPass = MyWifiPass;
 
@@ -101,92 +194,7 @@ void setup() {
 
   // Establish SI7021
   sensor.begin();
-
-  //
-
 }
-
-static unsigned long epochTime;
-static time_t localTime, utc;
-static TimeChangeRule usEDT = {"EDT", Second, Sun, Mar, 2, -300};  //UTC - 5 hours - change this as needed
-static TimeChangeRule usEST = {"EST", First, Sun, Nov, 2, -360};   //UTC - 6 hours - change this as needed
-Timezone usEastern(usEDT, usEST);
-const char * ampm[] = {"AM", "PM"} ;
-void establishLocalTime() {
-  if (WiFi.status() == WL_CONNECTED) { 
-    Serial.println("Getting internet time");  
-    timeClient.update();
-    epochTime =  timeClient.getEpochTime();
-    utc = epochTime;
-    localTime = usEastern.toLocal(utc);
-    Serial.print("Current time is ");Serial.print(hourFormat12(localTime));
-    Serial.print(":");Serial.print(minute(localTime));
-    Serial.print(":");Serial.print(second(localTime));
-    Serial.print(" ");Serial.println(ampm[isPM(localTime)]);
-    lastSync = currentms/1000;
-  }
-  else
-    Serial.println("No internet connection");
-}
-
-char convertHexToChar(byte in) {
-  if (in < 10)
-    return in+48;
-   else
-    return in+55;
-}
-
-void capturePushButton() {
-  pushButtonPressed = true;
-}
-
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    // If you do not want to use a username and password, change next line to
-    // if (client.connect("ESP8266Client")) {
-    if (client.connect("ESP8266Client")) {
-      Serial.println("connected");
-      client.subscribe("ledToggle");
-      client.subscribe("reset");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
-}
-
-static byte ledToggleState = 1;
-void callback(char* topic, byte* thisPayload, unsigned int lPayload) {
-  // ESP.restart();
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  if (topic[0] == 'r' && topic[1] == 'e' && topic[2] == 's' && topic[3] == 'e' && topic[4] == 't')
-  {
-    Serial.println("Reseting ESP in 3 seconds...");
-    delay(3000);
-    ESP.restart();
-  }
-  else {
-    for (int i = 0; i < lPayload; i++) {
-      char receivedChar = (char)thisPayload[i];
-      Serial.print(receivedChar);
-      if (receivedChar == '0')
-        ledToggleState = 0;
-      if (receivedChar == '1')
-        ledToggleState = 1;
-    }
-    Serial.println();
-  }
-}
-
-//=============================================================
 
 // Time tracking variables
 unsigned long prevBlink_ms = 0;
@@ -198,7 +206,6 @@ const long broadcastRate = 3000;
 unsigned long lastPress_ms = 0;
 unsigned long prevSynchTime_ms = 0;
 const int synchTimeRate = 60*1000;
-
 
 void loop() {
   // get current time tick
