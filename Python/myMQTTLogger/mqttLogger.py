a3cffef4aacc8367fd743e37e8886e948f1e84f5
tpwright <tpwright@ncsu.edu>
2019-05-01 16:56:45 -0400

    working on python mqtt

--- Python/myMQTTLogger/mqttLogger.py
+++ Python/myMQTTLogger/mqttLogger.py
@@ -4,12 +4,29 @@ import time
 import paho.mqtt.client as paho
 broker="192.168.1.25"
 
+# Read sensorList and update sensor subscriptions
+def updateSubscriptions():
+    subscriptionListFile = open("subscriptionList.txt","r")
+    subscriptionList = subscriptionListFile.readlines()
+    for i in range(len(subscriptionList)):
+        subscriptionList[i] = subscriptionList[i].rstrip("\n\r")
+    return subscriptionList
+
 #define callback
 def on_message(client, userdata, message):
     time.sleep(1)
-    print("received message =",str(message.payload.decode("utf-8")))
+    print("received message from {} = {}".format(message.topic.decode("utf-8"),message.payload.decode("utf-8")))
+    # print('We are the {} who say "{}!"'.format('knights', 'Ni'))
 
-client= paho.Client("client-001") #create client object client1.on_publish = on_publish #assign function to callback client1.connect(broker,port) #establish connection client1.publish("house/bulb1","on")
+#########################################################################
+subscriptionList = updateSubscriptions()
+print(subscriptionList)
+
+client= paho.Client("client-001")
+#create client object client1.on_publish = on_publish
+#assign function to callback client1.connect(broker,port)
+#establish connection client1.publish("house/bulb1","on")
+
 ######Bind function to callback
 client.on_message=on_message
 #####
@@ -17,10 +34,11 @@ print("connecting to broker ",broker)
 client.connect(broker)#connect
 client.loop_start() #start loop to process received messages
 print("subscribing ")
-client.subscribe("testSensors/T")#subscribe
+for i in range(len(subscriptionList)):
+    client.subscribe(subscriptionList[i]) #subscribe
 time.sleep(2)
 # print("publishing ")
 #client.publish("house/bulb1","on")#publish
 #time.sleep(4)
-client.disconnect() #disconnect
-client.loop_stop() #stop loop
+#client.disconnect() #disconnect
+client.loop_forever() #stop loop
